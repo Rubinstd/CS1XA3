2b54754c9bff6b66223ab781547d11f89974374d
Daniel Rubinstein <rubinstd@mills.cas.mcmaster.ca>
2018-02-21 11:42:56 -0500

    Reformated ProjectAnalyze to use functions, and editted the check_haskell_errors to use &> instead of just >

--- Assign1/ProjectAnalyze.sh
+++ Assign1/ProjectAnalyze.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+check_uptodate(){
 git fetch
 git diff master origin/master
+}
 
+check_uncommited(){
 git diff>changes.log
+}
 
+create_todo_list(){
 grep -r "#TODO">todo.log . --exclude "todo.log" --exclude "ProjectAnalyze.sh"
+}
 
-find -name "*.hs" -exec ghc -fno-code {} \;>error.log 
+check_haskell_errors(){
+find -name "*.hs" -exec ghc -fno-code {} \;&>error.log
+}
+
+
+check_uptodate
+check_uncommited
+create_todo_list
+check_haskell_errors
+
