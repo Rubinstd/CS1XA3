dd34d28c770952caa82e9a4e7ed54972927e60f2
Daniel Rubinstein <rubinstd@mills.cas.mcmaster.ca>
2018-02-21 12:11:18 -0500

    Added custom function that allows user to search git log for commit messages containing a given string.

--- Assign1/ProjectAnalyze.sh
+++ Assign1/ProjectAnalyze.sh
@@ -17,9 +17,21 @@ check_haskell_errors(){
 find -name "*.hs" -exec ghc -fno-code {} \;&>error.log
 }
 
+search_log(){
+
+git log | grep "$1" -B 4 -A 1 > search.log
+
+}
 
 check_uptodate
 check_uncommited
 create_todo_list
 check_haskell_errors
 
+if [ $# -eq 2  ]
+then
+	if [ "$1" = "sl" ]
+	then
+		search_log $2
+	fi
+fi
