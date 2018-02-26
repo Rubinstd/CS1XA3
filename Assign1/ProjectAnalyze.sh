b45a83e3a595e0509f41f526bfc67c8930e1f560
Daniel Rubinstein <rubinstd@mills.cas.mcmaster.ca>
2018-02-26 16:56:19 -0500

    Added search features for the script and updated how the error logs are formated. Also edited the readme to fit the new features

--- Assign1/ProjectAnalyze.sh
+++ Assign1/ProjectAnalyze.sh
@@ -1,64 +1,105 @@
 #!/bin/bash
 
+#Uses git status to check if the local repo is up to date with the remote repo.
 check_uptodate(){
-git status > tmpstatus.txt
+git status > tmpstatus.txt #Don't forget to take out the writing thing
 }
 
+#Checks for all uncommited changes and puts them into changes.log. 
+#This function overwrites any previous info in changes.log to reduce clutter.
 check_uncommited(){
 git diff HEAD --exclude "changes.log" >changes.log
 }
 
+#Greps the entire git repository (assuming the script is at the root) and puts all lines with #TODO into a todo.log.
+#This function overwrites any previous info in todo.log to reduce clutter.
 create_todo_list(){
 grep -r "#TODO" . --exclude={todo.log,ProjectAnalyze.sh,changes.log}>todo.log
 }
 
+#Checks all .hs files for syntax errors. Additionally, it will check if there is a main in all haskell files and add an undefined main if needed.
 check_haskell_errors(){
-
-#find . -name "*.hs" -type f | grep "main" | -ghc -fno-code >error.log
 
 echo "" > error.log
 
-find . -name "*.hs" | while read line; do
-	echo "Errors for $line :" >> error.log
+find . -name "*.hs" -type f| while read line; do
+	echo "Errors for \"$line\" :" >> error.log
 	maincount=$(grep "main" "$line" | wc -l)
 	if [ $maincount -eq 0 ]
 	then
 		echo "main = undefined">>$line
 	fi
 	ghc -fno-code "$line" 1>/dev/null 2>>error.log
+	echo "" >>error.log
 		
 done
 
 }
 
+#Checks alls .py files for python syntax errors.
+check_python_errors(){
+echo "" >error_python.log
+find . -name "*.py" -type f | while read line; do
+	echo "Errors for \"$line\" :" >>error_python.log
+	echo "" >>error_python.log
+	python -m py_compile "$line"&>>error_python.log
+	echo "" >>error_python.log
+done
+}
+
+#Searches through git log for a given search string. Requires the search string as an input.
 search_log(){
 
 git log | grep "$1" -B 4 -A 1 > search.log
 
 }
 
+#Searches through git log for a given Authors commits. Requires the Author's name as an input.
 search_log_author(){
 
 git log | grep "Author: $1" -B 2 -A 3 > search.log
 
 }
 
+#Searches recursively through all files in all directories (assuming the script is in the root directory) for a given string.
+search_dir_string(){
+grep -r "$1" 
+}
+
+#Searches through all files in the directory (assuming the script is in the root directory) for files with a given search string in the name (this is not case sensitive)
+search_dir_file(){
+find -iname "*$1*"
+}
+
+#Runs all the standard check functions that you would naturally want to run if you were using the script just to analyze various aspects of your git repo.
 if [ $# -eq 0 ]
 then
 	check_uptodate
 	check_uncommited
 	create_todo_list
 	check_haskell_errors
-fi
+	check_python_errors
 
-if [ $# -eq 2  ]
+
+#Runs any indicated extra parameter functions based on the inputs provided.
+elif [ $# -eq 2  ]
 then
 	if [ "$1" = "sl" ]
 	then
 		search_log $2
-	fi
-	if [ "$1" = "sla" ]
+	
+	elif [ "$1" = "sla" ]
 	then
 		search_log_author $2
+	elif [ "$1" = "sds" ]
+	then
+		search_dir_string $2
+	elif [ "$1" = sdf ]
+	then
+		search_dir_file $2
+	else
+		echo "Invalid inputs"
 	fi
+else
+	echo "Invalid number of inputs"
 fi
--- Assign1/README.md
+++ Assign1/README.md
@@ -1,7 +1,7 @@
 Assignment 1 - Project Analyzer
 
 Assignment Required Features:
-1. Uses a git fetch and compares the local repo to the remote repo.
+1. Uses git status to compare the local repo to the remote repo.
 2. Puts the output of git diff into changes.log in order to show any uncommited changes.
 3. Greps for #TODO within any files (excluding the todo.log and ProjectAnalyze.sh) and places the results into todo.log.
 4. Searches for haskell files, and checks them for any errors, placing the output into error.log.
@@ -12,9 +12,15 @@ Custom Features:
    into a file called search.log.
 2. Sometimes you're working on these projects in a group. Now you're obviously a pro programmer who never messes things up, but unfortunately I can't say the same for your buddies.
    By using the two arguments sla "authorname" (sla standing for search log by author), the script will place only the commits whose authors are the given name into the search.log file
-   that way you can check what those pesky peers of yours have done to your beautiful code.
+   that way you can check what those pesky peers of yours have done to your beautiful code. (Note: Both this and the first function can be done through git commands, I just wanted to add
+   them in my own way so that I could actually edit which information gets found and how if I ever wanted to change the formatting myself)
 3. This is a custom function embedded into my haskell error checking function. Instead of just blindly checking for haskell errors, I first check to see if the hs file has a main function. 
    If it does not, I add in a line at the end of the file with an undefined main. This allows the error checker to then continue to check the rest of the code for any errors. 
    I also made sure that the stdout (which is normally just a couple of lines that say that the file has compiled the main) aren't printed while the script runs or in the error.log as I 
    found them quite useless. Only the stderr (the actual error messages) are sent to error.log. Lastly I made sure that there is a header for each hs file indicating it's directory to show 
    that the error messages after said line are for the given files.
+4. The script contains a function that works the same as the haskell error checking function but for python files.
+5. Added in a built in search through all files for a search string feature when you give the script the inputs sds "searchstring" (sds standing for search directories for string). This is 
+   simply a function I added because programmers are lazy and typing out a whole grep statement can be tedious.
+6. Added a built in search for files with a search string in the name when you give the script the inputs sdf "filename". This search is not case sensitive and returns all files (and their
+   directories). This function is useful because typing out the whole find statement can be tedious and programmers are lazy.
